b02255dfb8a849ecc280abc5917389c91156c1ef
Sean Reddell <sreddell@csc301x14.ad.calpoly.edu>
2016-02-16 18:00:49 -0800

    Lays outline for lab.\n\nMy_sqrt() still to be implemented.

---
+++ lab5/hello.c
@@ -0,0 +1,25 @@
+//Sean Reddell
+//Shiv Sulkar
+//CPE225
+//Lab 5
+
+#include <stdio.h>
+
+float my_sqrt(float r);
+
+int main
+  {
+    //TEST CASE 1
+    float x = my_sqrt(2.0);
+    printf("Root of 2.0: %f\n", x);
+    
+    //TEST CASE 2
+    x = my_sqrt(5.0);
+    printf("Root of 5.0: %f\n", x);
+    
+    //TEST CASE 3
+    x = my_sqrt(25.0);
+    printf("Root of 25.0: %f\n", x);
+    
+    return 0;
+  }
